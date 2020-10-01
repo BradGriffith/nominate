2d6dd59be91f83f8ea051b14461e48771ea654b1
Brad Griffith <brad@buckeyeinteractive.com>
2020-10-01 18:07:12 -0400

    Fix votes foreign key referencing positions instead of nominations

--- database/migrations/2020_10_01_060833_create_votes.php
+++ database/migrations/2020_10_01_060833_create_votes.php
@@ -22,7 +22,7 @@ class CreateVotes extends Migration
             $table->timestamps();
             $table->foreign('position_id')->references('id')->on('positions');
 
-            $table->foreign('nomination_id')->references('id')->on('positions');
+            $table->foreign('nomination_id')->references('id')->on('nominations');
         });
     }
 
