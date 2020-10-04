568a1706c3646b5907d8ce9c2e1396f5bd8b84a1
Brad Griffith <brad@buckeyeinteractive.com>
2020-10-03 20:01:30 -0400

    Move logic for identifying nominees ready for ranking from the controller to the model

--- app/Models/Nomination.php
+++ app/Models/Nomination.php
@@ -24,4 +24,29 @@ class Nomination extends Model
     public function ranks() {
       return $this->hasMany('App\Models\Ranking');
     }
+
+    public static function getNomineesForRanking($position_id = null)
+    {
+      $position_id = is_null($position_id) ? Position::getDefault()->id : $position_id;
+
+      $nominee_min_count = Position::find($position_id)->num_to_select;
+
+      //get the top $nominee_min_count nominees by number of votes
+      $nominees = static::withCount('votes')
+          ->where('position_id',$position_id)
+          ->where('year', date('Y'))
+          ->orderBy('votes_count','desc')
+          ->limit($nominee_min_count)
+          ->get();
+
+      // find the minimum number of votes you'd have to get to tie for last place
+      $min_votes = $nominees[min(count($nominees), $nominee_min_count)-1]->votes_count;
+
+      // get all nominees with at least as many votes as the nominee in $nominee_min_count-th place
+      return static::withCount('votes')
+          ->where('position_id',$position_id)
+          ->where('year', date('Y'))
+          ->having('votes_count','>=', $min_votes)
+          ->get();
+    }
 }
