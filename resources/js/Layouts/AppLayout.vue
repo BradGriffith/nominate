0908c33c5a81a526ec1b1fd79069ba4e7203d94a
Brad Griffith <brad@buckeyeinteractive.com>
2020-10-10 00:49:49 -0400

    Make Admin link open in the full window rather than a pop-up

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -26,9 +26,9 @@
                             <jet-nav-link href="/results" :active="$page.currentRouteName == 'results'">
                                 Results
                             </jet-nav-link>
-                            <jet-nav-link href="/admin">
+                            <a href="/admin" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                 Admin
-                            </jet-nav-link>
+                            </a>
                         </div>
                     </div>
 
