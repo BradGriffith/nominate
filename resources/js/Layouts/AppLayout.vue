6d4c576276450d463b387903e98f4e3d8e01e685
Brad Griffith <brad@buckeyeinteractive.com>
2020-10-11 06:35:00 -0700

    Only show the Admin link for logged-in users

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -26,7 +26,7 @@
                             <jet-nav-link href="/results" :active="$page.currentRouteName == 'results'">
                                 Results
                             </jet-nav-link>
-                            <a href="/admin" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <a href="/admin" v-if="$page.user" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                 Admin
                             </a>
                         </div>
